37d11405a3afc1a684e08b8e4ab7cad3da1fb7c2
amsabots <amsabots@gmail.com>
2021-12-01 21:55:02 +0300

    first commit

---
+++ src/main/java/com/amsabots/jenzi/fundi_service/FundiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.amsabots.jenzi.fundi_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundiServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FundiServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/amsabots/jenzi/fundi_service/FundiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amsabots.jenzi.fundi_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundiServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
