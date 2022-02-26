181ae73dd97807625b6a76446b67a48e2ef5b1ba
amsabots <amsabots@gmail.com>
2022-02-26 21:35:05 +0300

    chat system done

--- src/main/java/com/amsabots/jenzi/fundi_service/repos/TagCategoryRepo.java
+++ src/main/java/com/amsabots/jenzi/fundi_service/repos/TagCategoryRepo.java
@@ -2,6 +2,7 @@ package com.amsabots.jenzi.fundi_service.repos;
 
 import com.amsabots.jenzi.fundi_service.entities.CategoryTags;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,5 +12,6 @@ public interface TagCategoryRepo extends JpaRepository<CategoryTags, Long> {
 
     CategoryTags findByAccountIdAndTagId(long accountId, long tagId);
     List<CategoryTags> findAllByAccountId(long id);
+    @Query("delete from CategoryTags u where u.accountId=?2 and u.tagId=?1")
     void deleteByTagIdAndAccountId(long tagId, long accountId);
 }
