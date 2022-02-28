87940aa0d08842276c8bb621d7db3094475054af
amsabots <amsabots@gmail.com>
2022-02-28 21:07:52 +0300

    chat system done

--- src/main/java/com/amsabots/jenzi/fundi_service/repos/ChatRepo.java
+++ src/main/java/com/amsabots/jenzi/fundi_service/repos/ChatRepo.java
@@ -4,6 +4,7 @@ import com.amsabots.jenzi.fundi_service.entities.Chats;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -15,6 +16,7 @@ public interface ChatRepo extends JpaRepository<Chats, Long> {
 
     Page<Chats> findAllByChatRoomId(String chatRoomId, Pageable pageable);
     public Chats findChatsByMessageId(String messageId);
+    @Query(nativeQuery = true, value = "SELECT c from Chats c where c.chatRoomId = ?1 order by c.id DESC Limit 1")
     Chats findTopByOrderByChatRoomIdDesc(String roomId);
 
 }
