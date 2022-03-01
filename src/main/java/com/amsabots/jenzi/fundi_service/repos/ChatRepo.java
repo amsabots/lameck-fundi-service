ab4f5c161362e19fe13b339168556ffaa438f266
Andrew Mwebbi <andrew.mwebbi@abcgroup.local>
2022-03-01 11:08:20 +0300

    fixing from ABC

--- src/main/java/com/amsabots/jenzi/fundi_service/repos/ChatRepo.java
+++ src/main/java/com/amsabots/jenzi/fundi_service/repos/ChatRepo.java
@@ -7,6 +7,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 /**
  * @author andrew mititi on Date 2/2/22
  * @Project lameck-fundi-service
@@ -17,7 +19,7 @@ public interface ChatRepo extends JpaRepository<Chats, Long> {
     Page<Chats> findAllByChatRoomId(String chatRoomId, Pageable pageable);
     public Chats findChatsByMessageId(String messageId);
     @Query("SELECT c from Chats c where c.chatRoomId = ?1  order by c.id DESC")
-    org.hibernate.query.Query<Chats> findLastChatItemByChatRoomId(String roomId);
+    List<Chats> findLastChatItemByChatRoomId(String roomId);
 
 
 }
